5b2535092d0464fa3e97d13335f0a38624e4646f
machinecodeblue <david@machinecodeblue.com>
2016-06-23 09:17:52 -0400

    finished tutorial

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,6 +1,40 @@
 import { Component } from '@angular/core';
+import { RouteConfig, ROUTER_DIRECTIVES, ROUTER_PROVIDERS } from '@angular/router-deprecated';
+import { HeroService }     from './hero.service';
+import { HeroesComponent } from './heroes.component';
+import {DashboardComponent} from './dashboard.component'
+import {HeroDetailComponent} from './hero-detail.component'
 @Component({
   selector: 'my-app',
-  template: '<h1>My First Angular 2 App</h1>'
+  templateUrl:'app/app.component.html',
+  styleUrls:['app/app.component.css'],
+
+  directives: [ROUTER_DIRECTIVES],
+  providers: [
+    ROUTER_PROVIDERS, 
+    HeroService
+  ]
 })
-export class AppComponent { }
+@RouteConfig([
+  {
+    path: '/heroes',
+    name: 'Heroes',
+    component: HeroesComponent
+  },
+  {
+  path: '/dashboard',
+  name: 'Dashboard',
+  component: DashboardComponent,
+  useAsDefault: true
+},
+{
+  path: '/detail/:id',
+  name: 'HeroDetail',
+  component: HeroDetailComponent
+},
+
+
+])
+export class AppComponent {
+  title = 'Tour of Heroes';
+}
